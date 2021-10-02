fd23001c4ce3dbf645594b9937fc64255f092fc9
John McCormack <John.McCormack2@ibm.com>
2021-10-02 20:52:28 +0100

    debugging

--- working/temp.js
+++ working/temp.js
@@ -3,7 +3,7 @@ let i = 0
 
 async function main() {
     while (true) {
-        console.log("i: ", i++);
+        console.log("temp i: ", i++);
         await new Promise(resolve => setTimeout(resolve, 1000));
 
         if(i === 5)
--- working/timer.js
+++ working/timer.js
@@ -3,7 +3,7 @@ let i = 0
 
 async function main() {
     while (true) {
-        console.log("i: ", i++);
+        console.log("timer i: ", i++);
         await new Promise(resolve => setTimeout(resolve, 1000));
 
     }
